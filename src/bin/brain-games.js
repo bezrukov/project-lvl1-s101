6e61f24fa9ceb7e82b6ce35e2259b79396cc77f3
vbezrukov <virtus.at.honor@gmail.com>
2017-06-05 22:37:43 +0300

    test other modules in project

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
+
+import brainGames from '../';
 console.log('Welcome to the Brain Games!');
+
+brainGames();
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import readlineSync from 'readline-sync';
+
+export default () => {
+    const clientName = readlineSync.question('May I have you name ? ');
+
+    console.log(`Hello, ${clientName}`);
+}
