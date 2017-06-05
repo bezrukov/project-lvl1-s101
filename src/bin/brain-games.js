57dd87b0358c42501f200a8a57831d76677cfd23
vbezrukov <virtus.at.honor@gmail.com>
2017-06-05 23:28:49 +0300

    add eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-
 import brainGames from '../';
+
 console.log('Welcome to the Brain Games!');
 
 brainGames();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import readlineSync from 'readline-sync';
 
 export default () => {
-    const clientName = readlineSync.question('May I have you name ? ');
-
-    console.log(`Hello, ${clientName}`);
-}
+  const clientName = readlineSync.question('May I have you name ? ');
+  console.log(`Hello, ${clientName}`);
+};
